0b8202618dbffa8831a0a46744bae98896826e9c
niklasschmidt-glitch <niklasschmidt@live.com>
2026-09-01 11:21:43 +0200

    Add Safari Computer update metadata

---
+++ safari/VIRM_Assistant_Safari_Computer.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         VIRM Assistant – Safari Computer
+// @namespace    https://www.virtualmanager.com/
+// @version      1.00
+// @description  Planlæg træning og vis PP/KP på markeder og spillersider direkte i Virtual Manager på Mac.
+// @author       Niklas
+// @updateURL    https://github.com/niklasschmidt-glitch/virm-assistant/releases/latest/download/VIRM_Assistant_Safari_Computer.meta.js
+// @downloadURL  https://github.com/niklasschmidt-glitch/virm-assistant/releases/latest/download/VIRM_Assistant_Safari_Computer.user.js
+// @run-at       document-idle
+// @grant        none
+// @noframes
+// @match        https://www.virtualmanager.com/*
+// ==/UserScript==
